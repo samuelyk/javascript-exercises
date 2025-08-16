75e585f773692c43a35e170d67edb55eb35f64df
samuelyk <youngjoong.s.kim@gmail.com>
2025-08-16 16:31:21 -0700

    Concatenate strings

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,9 +8,17 @@
 */
 
 // Add your code right below, good luck!
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
 
+let greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old." 
 
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
+greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 
 // Do not change this
